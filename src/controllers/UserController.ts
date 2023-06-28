3b7eb81ae995b7d5493a9a2abefdd7c66a778a14
Lucas Veloso <lucasvelosoalves@gmail.com>
2023-06-28 16:07:10 -0300

    User Features

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,4 @@
 import { Request, Response, NextFunction  } from 'express'
-import { User } from '../models'
 import { compare, hash } from 'bcrypt'
 import { UserRepository } from '../repositories/UserRepository'
 
